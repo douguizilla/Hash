eaa83060f12666021d6a2cd674448c6ea2f50762
Douglas Gomes <douglasgommez@gmail.com>
2022-01-10 19:31:24 -0300

    criação das operações da tabela hash

---
+++ src/main/java/hash/server/HashTable.java
@@ -0,0 +1,38 @@
+package hash.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashTable {
+    private Map<String,String> hashTable = new HashMap<>();
+
+    public int add(String key, String value){
+        if (hashTable.containsKey(key)) {
+            hashTable.put(key,value);
+            return 1; //sucesso
+        }
+        return 0; //fracasso
+    }
+
+    public String remove(String key){
+        String value = hashTable.get(key);
+        if(value == null)
+            return null;
+
+        hashTable.remove(key);
+        return value;
+    }
+
+    public int update(String key, String value){
+        if (hashTable.containsKey(key)){
+            hashTable.replace(key,value);
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    public String read(String key){
+        return hashTable.get(key);
+    }
+}
