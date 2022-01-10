354d23603e31c3802cda6f5b52efca336db5344c
Douglas Gomes <douglasgommez@gmail.com>
2022-01-10 19:22:07 -0300

    atualização para receber chave e valor digitado no terminal

--- src/main/java/hash/client/HashTcpClient.java
+++ src/main/java/hash/client/HashTcpClient.java
@@ -5,13 +5,21 @@ import hash.Message;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class HashTcpClient {
 
     public static void main(String[] args) throws IOException {
         Socket client = new Socket("127.0.0.1",12345);
         ObjectOutputStream request = new ObjectOutputStream(client.getOutputStream());
-        Message p = new Message(0,7,"Douglas",5,"doido",-1);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Chave: ");
+        String key = input.next();
+
+        System.out.print("Valor: ");
+        String value = input.next();
+
+        Message p = new Message(0,key.length(),key,value.length(),value,-1);
         request.writeObject(p);
         request.close();
         client.close();
