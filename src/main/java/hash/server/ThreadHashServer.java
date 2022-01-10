f40fc5350282167ec9e30ea1ec37c2ee71962f67
Douglas Gomes <douglasgommez@gmail.com>
2022-01-10 19:22:34 -0300

    adicionada logica para ver se a mensagem estava sendo recebida

--- src/main/java/hash/server/ThreadHashServer.java
+++ src/main/java/hash/server/ThreadHashServer.java
@@ -1,5 +1,9 @@
 package hash.server;
 
+import hash.Message;
+
+import java.io.DataInputStream;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 
 public class ThreadHashServer extends Thread{
@@ -10,5 +14,15 @@ public class ThreadHashServer extends Thread{
 
     public void run(){
         //logica que o server vai fazer para o client
+        try{
+            ObjectInputStream entrance = new ObjectInputStream(client.getInputStream());
+            Message m = (Message) entrance.readObject();
+            System.out.println(m.toString());
+            entrance.close();
+            client.close();
+
+        }catch(Exception e){
+            System.out.println("Erro " + e.toString());
+        }
     }
 }
